2dd96c9b56337639ce26068ce76f87d9f21cdab8
BShabowski <benshabowski@gmail.com>
2023-09-17 22:34:31 -0500

    initial commit

---
+++ src/main/java/com/zgamelogic/application/App.java
@@ -0,0 +1,12 @@
+package com.zgamelogic.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.zgamelogic.controllers"})
+public class App {
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(App.class);
+        app.run(args);
+    }
+}
