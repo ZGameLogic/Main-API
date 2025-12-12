0e406c19939defa359aab00a2aafbf0795e7d235
Ben Shabowski <benshabowski@gmail.com>
2025-12-12 16:59:26 -0600

    Removing ghosts

--- src/main/java/com/zgamelogic/data/database/ghosty/aspect/AspectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zgamelogic.data.database.ghosty.aspect;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface AspectRepository extends JpaRepository<Aspect, Integer> {
-
-}
--- src/main/java/com/zgamelogic/data/database/ghosty/ghost/GhostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zgamelogic.data.database.ghosty.ghost;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface GhostRepository extends JpaRepository<Ghost, Integer> {
-
-}
